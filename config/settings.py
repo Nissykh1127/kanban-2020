d275a57e6b7011bed3f1b14ac8e18af710abbece
kyo hayashida <kanayan-love.1127@docomo.ne.jp>
2020-03-07 23:38:51 +0900

    make it better

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'hcwwi8%0n!g*g=k#^y-sw*w%coe5u3zm-2q_l6f=9^pjuodj7^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
